40ad784f4d8eeacf66f541fc90451264776dbc78
Dale Hunt <dalehunt@gmail.com>
2024-10-31 23:54:46 +0000

    - Animation Tweaks.

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,10 @@
 const HomeContainer_Headers = document.querySelectorAll('.HomeContainer > h1');
-const isPC = window.matchMedia('(min-width: 1024px)'); 
+const NavBarLinks = document.querySelectorAll('.NavBarContainer > a');
+const isPC = window.matchMedia('(min-width: 1024px)'); // Hack: PC Detection
 
 if (isPC) {
-    HomeContainer_Headers.forEach(header => { // Reminder: forEach Loop required for `document.querySelectorAll`
-        header.addEventListener('mouseenter', () => {
-            header.classList.add('ShouldColourPulse');
-        });
-
-        header.addEventListener('animationend', () => {
-            header.classList.remove('ShouldColourPulse');
-        });
+    HomeContainer_Headers.forEach(Header => {
+        Header.addEventListener('mouseenter', () => { Header.classList.add('ShouldColourPulse'); });
+        Header.addEventListener('animationend', () => { Header.classList.remove('ShouldColourPulse'); });
     });
 }
