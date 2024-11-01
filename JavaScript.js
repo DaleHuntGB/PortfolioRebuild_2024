e687a9224356d6b5fcd6dd245bafe30f8807115d
Dale Hunt <dalehunt@gmail.com>
2024-11-01 00:37:20 +0000

    Some styling.

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,26 @@
 const HomeContainer_Headers = document.querySelectorAll('.HomeContainer > h1');
-const NavBarLinks = document.querySelectorAll('.NavBarContainer > a');
-const isPC = window.matchMedia('(min-width: 1024px)'); // Hack: PC Detection
+const ShootingStarContainer = document.querySelector('.ShootingStarsContainer')
+const isPC = window.matchMedia('(min-width: 1024px)'); // PC Detection
 
-if (isPC) {
+if (isPC.matches) {
     HomeContainer_Headers.forEach(Header => {
         Header.addEventListener('mouseenter', () => { Header.classList.add('ShouldColourPulse'); });
         Header.addEventListener('animationend', () => { Header.classList.remove('ShouldColourPulse'); });
     });
+    setInterval(generateStar, 300);
+}
+
+function generateStar() {
+    const createdStar = document.createElement('div');
+    createdStar.classList.add('ShootingStar');
+    minStarSize = Math.ceil(2);
+    maxStarSize = Math.floor(5);
+    const size = Math.floor(Math.random() * (maxStarSize - minStarSize + 1)) + minStarSize;
+    createdStar.style.width = `${size}px`;
+    createdStar.style.height = `${size}px`;
+    createdStar.style.top = `${Math.random() * window.innerHeight}px`;
+    createdStar.style.left = `${Math.random() * window.innerWidth}px`;
+    createdStar.style.animationDuration = `${Math.random() * 1 + 1}s`;
+    ShootingStarContainer.appendChild(createdStar);
+    setTimeout(() => { createdStar.remove(); }, 1000);
 }
